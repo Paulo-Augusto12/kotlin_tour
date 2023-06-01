04a91055879b681cf430f250fa4d93fc2a7c007d
Paulo Augusto <paulo-augusto12@outlook.com>
2023-06-01 09:55:02 -0300

    feat: developed lambda exercise one

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,12 +5,39 @@ import functions.exercise_one.circleArea
 
 import functions.exercise_three.intervalInSeconds
 
+import lambda.exercise_one.urlsList
+
 fun main () {
-    val exerciseOne = circleArea(2)
 
-    val exerciseTwo = circleAreaSingleExpression(3)
+    val printDivider = " ------------------ [  ] ------------------ "
 
+    fun divider() {
+        println(printDivider)
+    }
 
-    println("Exercise 1  -> $exerciseOne")
-    println("Exercise 2 -> $exerciseTwo")
+    val functionsExerciseOne = circleArea(2)
+
+    val functionsExerciseTwo = circleAreaSingleExpression(3)
+
+    val functionsExerciseThree = intervalInSeconds(3, 15, 25)
+
+    val lambdaExerciseOne = urlsList()
+
+    divider()
+
+    println("Functions Exercise 1  -> $functionsExerciseOne")
+
+    divider()
+
+    println("Functions Exercise 2 -> $functionsExerciseTwo")
+
+    divider()
+
+    println("Functions Exercise 3 -> $functionsExerciseThree")
+
+    divider()
+
+    println("Lambda exercise 1 -> $lambdaExerciseOne")
+
+    divider()
 }
